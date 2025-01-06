5a2e8b3c6d5fa47df86251416838991259777519
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-06 11:14:31 +0000

    Integrate Checkov with Jenkins pipeline

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-resource "aws_security_group" "example" {
-    name        = "example"
-    description = "An example security group"
-
-    ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]  # Open to the world
-    }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-insecure-bucket"
+  acl    = "public-read"
 }
